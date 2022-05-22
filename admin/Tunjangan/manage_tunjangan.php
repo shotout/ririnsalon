6e926a76a77cb431e363ea508ef822964405a731
Sungut <renz.xanderverion@gmail.com>
2022-05-22 18:07:32 +0700

    sungut ganteng sekali v1.88

---
+++ admin/Tunjangan/manage_tunjangan.php
@@ -0,0 +1,251 @@
+<?php
+require_once('../../config.php');
+if(isset($_GET['id']) && $_GET['id'] > 0){
+    $absensi_id = $_GET['id'];
+    $qry = $conn->query("SELECT absensi.id,id_karyawan,nama,bulan,hadir,absen,lembur,izin FROM karyawan JOIN absensi ON karyawan.id=absensi.id_karyawan WHERE absensi.id = '$absensi_id'");
+    if($qry->num_rows > 0){
+        foreach($qry->fetch_assoc() as $k => $v){
+            $$k=$v;
+        }
+    }
+}
+?>
+
+<style>
+    select[readonly].select2-hidden-accessible + .select2-container {
+        pointer-events: none;
+        touch-action: none;
+        background: #eee;
+        box-shadow: none;
+    }
+
+    select[readonly].select2-hidden-accessible + .select2-container .select2-selection {
+        background: #eee;
+        box-shadow: none;
+    }
+</style>
+<div class="card card-outline card-primary">
+    <div class="card-header">
+        <h4 class="card-title">Data Absensi Karyawan</h4>
+    </div>
+    <div class="card-body">
+        <form action="" id="absensi-form">
+            <input type="hidden" name="absensi_id" value="<?php echo isset($absensi_id) ? $absensi_id : '' ?>">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        <label class="control-label text-info">ID Karyawan</label>
+
+						<div class="row-md-1">
+						<input type="text" name = "text_id_karyawan" id = "text_id_karyawan" class="form-control rounded-0" readonly value= "<?php echo isset($id_karyawan) ? $id_karyawan : ''  ?>"></input>
+						</div>                       
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="karyawan" class="control-label text-info">Karyawan</label>
+                            <select name="nama_karyawan" id="nama_karyawan" class="custom-select select2">
+                                                        
+                            <option <?php echo !isset($id_karyawan) ? 'selected' : '' ?>disabled></option>
+
+							<?php 
+                            $karyawan = $conn->query("SELECT id,nama FROM karyawan ORDER BY id ASC");
+                            while($row=$karyawan->fetch_assoc()):
+                            ?>
+                            							
+                            <option value="<?php echo $row['id'] ?>" 
+                            <?php echo isset($id) && $id == $row['id'] ? 'selected disabled' : "" ?>><?php echo $row['nama'] ?>
+
+							</option>							
+                            <?php endwhile;  ?>							
+                            </select>							
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <fieldset>
+                    <legend class="text-info"></legend>
+                    <div class="row justify-content-center align-items-end">
+                            
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label for="bulan" class="control-label">Bulan</label>
+                                <select  name = "bulan" id="bulan" class="custom-select select2">
+                                    <option selected></option>
+                                    
+                                    <option value="1" 
+                                    <?php echo isset($bulan) && $bulan == 1 ? 'selected': '' ?>>Januari</option>
+                                     
+                                    <option value="2" 
+                                    <?php echo isset($bulan) && $bulan == 2 ? 'selected': '' ?>>Februari</option>
+
+                                    <option value="3" 
+                                    <?php echo isset($bulan) && $bulan == 3 ? 'selected': '' ?>>Maret</option>
+
+                                    <option value="4" 
+                                    <?php echo isset($bulan) && $bulan == 4 ? 'selected': '' ?>>April</option>
+
+                                    <option value="5" 
+                                    <?php echo isset($bulan) && $bulan == 5 ? 'selected': '' ?>>Mei</option>
+
+                                    <option value="6" 
+                                    <?php echo isset($bulan) && $bulan == 6 ? 'selected': '' ?>>Juni</option>
+
+                                    <option value="7" 
+                                    <?php echo isset($bulan) && $bulan == 7 ? 'selected': '' ?>>Juli</option>
+
+                                    <option value="8" 
+                                    <?php echo isset($bulan) && $bulan == 8 ? 'selected': '' ?>>Agustus</option>
+
+                                    <option value="9" 
+                                    <?php echo isset($bulan) && $bulan == 9 ? 'selected': '' ?>>September</option>
+
+                                    <option value="10" 
+                                    <?php echo isset($bulan) && $bulan == 10 ? 'selected': '' ?>>Oktober</option>
+
+                                    <option value="11" 
+                                    <?php echo isset($bulan) && $bulan == 11 ? 'selected': '' ?>>November</option>
+
+                                    <option value="12" 
+                                    <?php echo isset($bulan) && $bulan == 12 ? 'selected': '' ?>>Desember</option>
+                                    
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label for="unit"  class="control-label">Hadir</label>
+                                <input type="number"  class="form-control rounded-0" id="hadir" name ="hadir" value="<?php echo isset($hadir) ? $hadir : ''; ?>">
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label for="unit"  class="control-label">Absen</label>
+                                <input type="number"  class="form-control rounded-0" id="absen" name="absen" value="<?php echo isset($absen) ? $absen : ''; ?>">
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label for="qty" class="control-label">Lembur</label>
+                                <input type="number" step="any" class="form-control rounded-0" id="lembur" name="lembur" value="<?php echo isset($lembur) ? $lembur : ''; ?>">
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label for="qty" class="control-label">Izin</label>
+                                <input type="number" step="any" class="form-control rounded-0" id="izin" name="izin" value="<?php echo isset($izin) ? $izin : ''; ?>">
+                            </div>
+                        </div>
+                        <!-- <div class="col-md-2 text-center">
+                            <div class="form-group">
+                                <button type="button" class="btn btn-flat btn-sm btn-primary" id="add_to_list">Add to List</button>
+                            </div>
+                        </div> -->
+                </fieldset>
+                <hr>
+                
+    <!-- <div class="card-footer py-1 text-center">
+        <button class="btn btn-flat btn-primary" type="submit" form="absensi-form">Save</button>
+        <a class="btn btn-flat btn-dark" href="<?php echo base_url.'/admin?page=absensi/absensi' ?>">Cancel</a>
+    </div> -->
+</div>
+
+<script>
+        
+	$('#nama_karyawan').change(function(){
+		tes = $('#nama_karyawan').val();
+		// $('#text_id_karyawan').text(tes);
+        $("#text_id_karyawan").val(tes);
+	})
+
+    
+	    $(function(){
+        $('.select2').select2({
+            placeholder:"Pilih karyawan",
+            width:'resolve',
+        })
+        $('#bulan').select2({
+            placeholder:"Pilih Bulan",
+            width:'resolve',
+        })
+    })
+        
+
+        
+        $('#absensi-form').submit(function(e){
+			e.preventDefault();
+            var _this = $(this)
+			 $('.err-msg').remove();
+			start_loader();
+			$.ajax({
+				url:_base_url_+"classes/Master.php?f=save_absensi",
+				data: new FormData($(this)[0]),
+                cache: false,
+                contentType: false,
+                processData: false,
+                method: 'POST',
+                type: 'POST',
+                dataType: 'json',
+				error:err=>{
+					console.log(err)
+					alert_toast("An error occured",'error');
+					end_loader();
+				},
+				success:function(resp){
+					if(resp.status == 'success'){
+						location.replace(_base_url_+"admin/?page=absensi/absensi");
+					}else if(resp.status == 'failed' && !!resp.msg){
+                        var el = $('<div>')
+                            el.addClass("alert alert-danger err-msg").text(resp.msg)
+                            _this.prepend(el)
+                            el.show('slow')
+                            end_loader()
+                    }else{
+						alert_toast("An error occured",'error');
+						end_loader();
+                        console.log(resp)
+					}
+                    $('html,body').animate({scrollTop:0},'fast')
+				}
+			})
+		})
+
+    //     if('<?php echo isset($id) && $id > 0 ?>' == 1){
+    //         calc()
+    //         $('#supplier_id').trigger('change')
+    //         $('#supplier_id').attr('readonly','readonly')
+    //         $('table#list tbody tr .rem_row').click(function(){
+    //             rem($(this))
+    //         })
+    //     }
+    // })
+    // function rem(_this){
+    //     _this.closest('tr').remove()
+    //     calc()
+    //     if($('table#list tbody tr').length <= 0)
+    //         $('#supplier_id').removeAttr('readonly')
+
+    // }
+    // function calc(){
+    //     var sub_total = 0;
+    //     var grand_total = 0;
+    //     var discount = 0;
+    //     var tax = 0;
+    //     $('table#list tbody input[name="total[]"]').each(function(){
+    //         sub_total += parseFloat($(this).val())
+            
+    //     })
+    //     $('table#list tfoot .sub-total').text(parseFloat(sub_total).toLocaleString('en-US',{style:'decimal',maximumFractionDigit:2}))
+    //     var discount =   sub_total * (parseFloat($('[name="discount_perc"]').val()) /100)
+    //     sub_total = sub_total - discount;
+    //     var tax =   sub_total * (parseFloat($('[name="tax_perc"]').val()) /100)
+    //     grand_total = sub_total + tax
+    //     $('.discount').text(parseFloat(discount).toLocaleString('en-US',{style:'decimal',maximumFractionDigit:2}))
+    //     $('[name="discount"]').val(parseFloat(discount))
+    //     $('.tax').text(parseFloat(tax).toLocaleString('en-US',{style:'decimal',maximumFractionDigit:2}))
+    //     $('[name="tax"]').val(parseFloat(tax))
+    //     $('table#list tfoot .grand-total').text(parseFloat(grand_total).toLocaleString('en-US',{style:'decimal',maximumFractionDigit:2}))
+    //     $('[name="amount"]').val(parseFloat(grand_total))
+
+    // }
+</script>
---
+++ admin/Tunjangan/tunjangan.php
@@ -0,0 +1,138 @@
+<div class="card card-outline card-primary">
+	<div class="card-header">
+		<h3 class="card-title">Daftar Absensi Karyawan</h3>
+		<div class="card-tools">
+		<a href="javascript:void(0)" id="create_new" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Tambah Data Absensi</a>
+		</div>
+	</div>
+	<div class="card-body">
+		<div class="container-fluid">
+        <div class="container-fluid">
+			<table class="table table-hovered table-striped">
+				<colgroup>
+					<col width="5%">
+					<col width="20%">
+					<col width="20%">
+					<col width="13%">
+					<col width="13%">
+					<col width="13%">
+					<col width="13%">
+				</colgroup>
+				<thead>
+					<tr>
+						<th>ID</th>						
+						<th>Nama</th>						
+						<th>Bulan</th>
+                        <th>Hadir</th>                        
+                        <th>Absen</th>
+						<th>Lembur</th>
+						<th>Izin</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php 
+					$i = 1;
+						$qry = $conn->query("SELECT absensi.id,id_karyawan,nama,bulan,hadir,absen,lembur,izin FROM karyawan JOIN absensi ON karyawan.id=absensi.id_karyawan order by id_karyawan asc ");
+						while($row = $qry->fetch_assoc()):
+					?>
+						<tr>
+							<!-- <td class="text-center"><?php echo $i++; ?></td> -->
+                            <td><?php echo $row['id_karyawan'] ?></td>	
+                            <td><?php echo $row['nama'] ?></td>                            
+                            <td>
+								<?php if($row['bulan'] == 1): ?>
+                                    <span>Januari</span>
+                                <?php elseif($row['bulan'] == 2): ?>
+                                    <span>Februari</span>
+									<?php elseif($row['bulan'] == 3): ?>
+                                    <span>Maret</span>
+									<?php elseif($row['bulan'] == 4): ?>
+                                    <span>April</span>
+									<?php elseif($row['bulan'] == 5): ?>
+                                    <span>Mei</span>
+									<?php elseif($row['bulan'] == 6): ?>
+                                    <span>Juni</span>
+									<?php elseif($row['bulan'] == 7): ?>
+                                    <span>Juli</span>
+									<?php elseif($row['bulan'] == 8): ?>
+                                    <span>Agustus</span>
+									<?php elseif($row['bulan'] == 9): ?>
+                                    <span>September</span>
+									<?php elseif($row['bulan'] == 10): ?>
+                                    <span>Oktober</span>
+									<?php elseif($row['bulan'] == 11): ?>
+                                    <span>November</span>
+                                <?php else :?>
+                                    <span>Desember</span>
+                                <?php endif; ?>
+								
+							</td>
+							<td><?php echo $row['hadir'] ?> Hari</td>
+							<td><?php echo $row['absen'] ?> Hari</td>
+							<td><?php echo $row['lembur'] ?> Hari</td>
+							<td><?php echo $row['izin'] ?> Hari</td>	
+                                                     	
+																				
+							
+
+							<td align="center">
+								 <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
+				                  		Action
+				                    <span class="sr-only">Toggle Dropdown</span>
+				                  </button>
+				                  <div class="dropdown-menu" role="menu">				                   
+				                    <div class="dropdown-divider"></div>
+				                    <a class="dropdown-item edit_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
+				                    <div class="dropdown-divider"></div>
+				                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
+				                  </div>
+							</td>
+						</tr>
+					<?php endwhile; ?>
+				</tbody>
+			</table>
+		</div>
+		</div>
+	</div>
+</div>
+<script>
+	$(document).ready(function(){
+		$('.delete_data').click(function(){
+			_conf("Yakin ingin menghapus data absensi karyawan ini?","delete_category",[$(this).attr('data-id')])
+		})
+		$('#create_new').click(function(){
+			uni_modal("<i class='fa fa-plus'></i> Tambah Data Absensi Karyawan","absensi/manage_absensi.php","mid-large")
+		})
+		$('.edit_data').click(function(){
+			uni_modal("<i class='fa fa-edit'></i> Edit Data Absensi Karyawan","absensi/manage_absensi.php?id="+$(this).attr('data-id'),"mid-large")
+		})
+		// $('.view_data').click(function(){
+		// 	uni_modal("<i class='fa fa-box'></i> Detail Data Karyawan","karyawan/view_karyawan.php?id="+$(this).attr('data-id'),"")
+		// })
+		$('.table td,.table th').addClass('py-1 px-2 align-middle')
+		$('.table').dataTable();
+	})
+	function delete_category($id){
+		start_loader();
+		$.ajax({
+			url:_base_url_+"classes/Master.php?f=delete_absensi",
+			method:"POST",
+			data:{id: $id},
+			dataType:"json",
+			error:err=>{
+				console.log(err)
+				alert_toast("An error occured.",'error');
+				end_loader();
+			},
+			success:function(resp){
+				if(typeof resp== 'object' && resp.status == 'success'){
+					location.reload();
+				}else{
+					alert_toast("An error occured.",'error');
+					end_loader();
+				}
+			}
+		})
+	}
+</script>
