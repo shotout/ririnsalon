c806e0efb597c425f37c7c5aff7fc173726e11e3
shotout <hevaldo66@gmail.com>
2022-05-22 18:27:28 +0700

    update

--- admin/clients/index.php
+++ admin/clients/index.php
@@ -1,6 +1,6 @@
 <?php if($_settings->chk_flashdata('success')): ?>
 <script>
-	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success2')
+	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
 </script>
 <?php endif;?>
 
